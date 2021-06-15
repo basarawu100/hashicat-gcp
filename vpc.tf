c2c64d906f417aabfb798bae654f4e24f5e5eade
basara wu <basarawu100@gmail.com>
2021-06-15 09:06:10 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -6,5 +6,5 @@ module "network" {
 
   network_name = "gaurav-network"
   project_id = "var.project"
-  subnets = [   {     subnet_name   = "gaurav-subnet"     subnet_ip     = "10.100.10.0/24"     subnet_region = var.region   } ]
+  subnets = "subnets = [   {     subnet_name   = "gaurav-subnet"     subnet_ip     = "10.100.10.0/24"     subnet_region = var.region   } ]"
 }
